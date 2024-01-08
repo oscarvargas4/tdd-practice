29dd780465908d1f78a8fb5c73301e57da51a24d
Oscar Vargas <oscar.andres.vargas1@gmail.com>
2024-01-08 16:40:12 +0100

    Basic fizzbuzz tests completed

--- test/fizzbuzz.test.js
+++ test/fizzbuzz.test.js
@@ -4,15 +4,13 @@ const fizzbuzz = (number) => {
   if (typeof number !== 'number') throw new Error('Parameter should be a number')
   if (Number.isNaN(number)) throw new Error('Parameter should be a number')
 
+  if (number % 3 === 0 && number % 5 === 0) return 'fizzbuzz'
   if (number % 3 === 0) return 'fizz'
+  if (number % 5 === 0) return 'buzz'
   return number
 }
 
 describe('fizzbuzz', () => {
-  it('should be a function', () => {
-    expect(typeof fizzbuzz).toBe('function')
-  })
-
   it('should throw error if not number provided as parameter', () => {
     expect(() => fizzbuzz()).toThrow()
   })
@@ -39,11 +37,17 @@ describe('fizzbuzz', () => {
 
   it('should return "fizz" if number provided is multiple of 3', () => {
     expect(fizzbuzz(3 * 2)).toBe('fizz')
-    expect(fizzbuzz(3 * 5)).toBe('fizz')
-    expect(fizzbuzz(3 * 10)).toBe('fizz')
+    expect(fizzbuzz(3 * 6)).toBe('fizz')
+    expect(fizzbuzz(3 * 9)).toBe('fizz')
   })
 
-  it('should return 4 if number provided is 4', () => {
-    expect(fizzbuzz(4)).toBe(4)
+  it('should return "buzz" if number provided is multiple of 5', () => {
+    expect(fizzbuzz(5 * 2)).toBe('buzz')
+    expect(fizzbuzz(5 * 5)).toBe('buzz')
+    expect(fizzbuzz(5 * 10)).toBe('buzz')
+  })
+
+  it('should return "fizzbuzz" if number provided is multiple of 5 & 3 at the same time', () => {
+    expect(fizzbuzz(5 * 3)).toBe('fizzbuzz')
   })
 })
